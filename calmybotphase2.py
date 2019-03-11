27a1da577f1b5a09c29c76dbdab81428c62c56fc
XiiaoZenth <39675645+kin9787@users.noreply.github.com>
2019-03-11 16:22:28 +0800

    Updated the codes.
    Not much changes just a few more functions.

--- calmybotphase2.py
+++ calmybotphase2.py
@@ -15,6 +15,7 @@ from discord.ext import commands
 BOT_PREFIX = "-"
 TOKEN = "NTQwNTA4NTk5MDM0ODM5MDUy.DzR8pw.NJFS4TovtDMvKe-FVymGBG-SL48"
 client = commands.Bot(command_prefix=BOT_PREFIX)
+client.remove_command('help')
 
 
 #######################################################################################################################
@@ -122,36 +123,42 @@ async def on_member_unban(server, user):
                               random.choice(ban_lifted_phrase))
 
 
+@client.event
+async def on_reaction_add(reaction, user):
+    channel = reaction.message.channel
+    await client.send_message(channel, "{0} has reacted to the message:\"{2}\" with  {1}  !".
+                              format(user.name, reaction.emoji, reaction.message.content))
+
+
+@client.event
+async def on_reaction_remove(reaction, user):
+    channel = reaction.message.channel
+    await client.send_message(channel, "{0} has removed the reaction \'{1}\' from the message: {2}.".
+                              format(user.name, reaction.emoji, reaction.message.content))
+
+
 #######################################################################################################################
 # command decorated functions
 
 # summon bot
-@client.command(pass_context=True)
+@client.command(pass_context=True, aliases=['summon', 'Summon'])
 async def join_voice(ctx):
-    author = ctx.message.author
-    voice_channel = author.voice_channel
-    await client.join_voice_channel(voice_channel)
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
 
 
 # disconnect bot
-@client.command(pass_context=True)
+@client.command(pass_context=True, aliases=['leave', 'bye', 'Leave'])
 async def leave_voice(ctx):
-    for x in client.voice_client:
-        if x.server == ctx.message.server:
-            return await x.disconnect()
-
-    return await client.say('I\'ll be back....')
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
 
 
-# quit bot
-@client.command(brief='Disconnects Calmy', description='Type command to disconnect Calmy from voice channel :(')
-async def disconnect():
-    print('Calmy Disconnected.')
-    await client.disconnect()
-
-
-@client.command(brief='Disconnects Calmy', description='Does what it stated.Logout Calmy.')
+@client.command(brief='Disconnects Calmy', description='Does what it stated.Logout Calmy.',
+                aliases=['logoff', 'shutdown'])
 async def logout():
+    await client.say('Calmy Out!!!')
     print('Calmy Out!!!')
     await client.logout()
 
@@ -175,6 +182,28 @@ async def joined(member: discord.Member):
     await client.say('{0.name}joined in {0.joined_at}'.format(member))
 
 
+@client.command(pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+
+    embed = discord.Embed(
+        colour=discord.Colour.orange()
+    )
+
+    embed.add_field(name='About Author', value='About the author.', inline=False)
+    embed.set_author(name='Help')
+    embed.add_field(name='Summon', value='Summons Calmy.', inline=False)
+    embed.add_field(name='Leave', value='Disconnect Calmy from voice channel.', inline=False)
+    embed.add_field(name='BitCoin', value='Check for BitCoin price.', inline=False)
+    embed.add_field(name='Temp', value='Check for current temperature.', inline=False)
+    embed.add_field(name='8ball', value='Test your luck with eight ball!', inline=False)
+    embed.add_field(name='Logout', value='Shut Down Calmy.', inline=False)
+
+    print('{0} needed help!'.format(author))
+
+    await client.send_message(author, embed=embed)
+
+
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # extra functions for bot
 
@@ -192,25 +221,59 @@ async def eight_ball(context):
                          'Alright, it says right here.....you\'re f**ked']
 
     await client.say(random.choice(possible_response) + ', ' + context.message.author.mention)
+    author = context.message.author
+    print('{0} spinned the 8 ball!!!'.format(author))
 
 
 # check for BTC price
-@client.command(brief='Check for BTC price.', aliases=['btc', 'BTC'])
-async def bitcoin():
+@client.command(pass_context=True, brief='Check for BTC price.', aliases=['btc', 'BTC', 'BitCoin'])
+async def bitcoin(ctx):
     url = 'http://api.coindesk.com/v1/bpi/currentprice/BTC.json'
     response = requests.get(url)
     value = response.json()['bpi']['USD']['rate']
     await client.say('Bitcoin price is: $' + value + '(USD)')
+    author = ctx.message.author
+    print('{0} has checked for BitCoin price.'.format(author))
 
 
 # check for temperature
-@client.command(brief='Check for temperature.')
-async def weather():
+@client.command(pass_context=True, brief='Check for temperature.', aliases=['temp', 'Temp'])
+async def temperature(ctx):
     url_weather = 'http://api.openweathermap.org/data/2.5/weather?q=Malaysia&&APPID=9c5be411701c5a3ef7de494fb9d0420d'
     response_weather = requests.get(url_weather)
     value_weather = response_weather.json()['main']['temp']
     real_value = str(value_weather)
     await client.say('Temperature for today is ' + real_value + ' F ')
+    author = ctx.message.author
+    print('{0} has checked the temperature.'.format(author))
+
+
+# check author details
+@client.command(pass_context=True, aliases=['about author', 'AboutAuthor', 'aboutauthor', 'About Author'])
+async def about_author():
+    embed = discord.Embed(
+        title='Some info about the creator.',
+        description='Created Calmy on 30/1/2019',
+        colour=discord.Colour.purple()
+    )
+
+    embed.set_footer(text='© 2019 Cereal Monster')
+    embed.set_image(url='https://vignette.wikia.nocookie.net/pyruslords/images/5/51/Derp_Face.png/revision'
+                        '/latest?cb=20130515193137')
+    embed.set_thumbnail(url='https://vignette.wikia.nocookie.net/pyruslords/images/5/51/Derp_Face.png/revision'
+                            '/latest?cb=20130515193137')
+    embed.set_author(name='Cereal Monster',
+                     icon_url='https://vignette.wikia.nocookie.net/pyruslords/images/5/51/Derp_Face.png/revision'
+                              '/latest?cb=20130515193137')
+    embed.add_field(name='Real Name:', value='You will need to PM me.', inline=False)
+    embed.add_field(name='Email:', value='xiiaozenth@gmail.com', inline=False)
+    embed.add_field(name='Github Profile Link:', value='https://github.com/kin9787', inline=True)
+    embed.add_field(name='Check out my full code on Github!', value='https://github.com/kin9787/Calmy-Discord-Bot.git',
+                    inline=True)
+
+    await client.say(embed=embed)
+
+    print('Someone called the master!!!')
 
 
 #######################################################################################################################
